6789aad4536da4abbf811323106c96d8a0eca8f6
Marcel <maschmidt.dev@gmail.com>
2022-07-24 00:18:24 +0200

    Fixed some bugs and text formatting

--- loops.py
+++ loops.py
@@ -1,32 +1,45 @@
-greetings = ["hello!", "hi!", "greetings!" , "hello" , "hi" , "greetings" ]
-goodbyes = ["bye." , "goodbye" , "see you!" , "later!"]
-responses = ["i like singing. do you like something about music?" , "i'm learning coding. i hope i will succeed. how is your coding?" , "would you play Magic the Gathering with me soon?"]
-answers = [ "fine", "ok" , "good" , "sure" , "yes" , "yea" , "yes i do"]
-negatives = ["no" , "nope" , "i don't like" ]
- 
-subjects = {"music" , "game" , "magic" , "song" , "singing" , "sing" , "coding" , "code" , "python"}
+from pprint import pprint
 import random
 
-user = input(random.choice (greetings))
+greetings = ["hello!", "hi!", "greetings!", "hello", "hi", "greetings"]
+goodbyes = ["bye.", "goodbye", "see you!", "later!"]
+responses = ["i like singing. do you like something about music?",
+             "i'm learning coding. i hope i will succeed. how is your coding?", "would you play Magic the Gathering with me soon?"]
+answers = ["fine", "ok", "good", "sure", "yes", "yea", "yes i do"]
+names = []
+negatives = ["no", "nope", "i don't like"]
+
+subjects = [
+    "music", "game", "magic", "song",
+    "singing", "sing", "coding", "code", "python"
+]
+
+user = input(random.choice(greetings) + "\n")
 user = user.lower()
 
 while (user != "no"):
+    print("loop starts here")
 
-   if (user in (greetings)):
-      new_answer = input ("who am i speaking to?")
-      answers.append(new_answer)
-      new_answer2 = input ("welcome " + (new_answer) + " wanna chat?")
-      answers.append (new_answer2)
-      user = input(random.choice (responses))
-      user = user.lower() 
-      if (user in (answers)):
-        new_answer3 = input ("good to know " + (random.choice (responses)))
-        answers.append(new_answer3)
+    if (user in greetings):
+        new_name = input("who am i speaking to?\n")
+        names.append(new_name)
+        new_answer = input("welcome " + (new_name) + ", wanna chat?\n")
+        answers.append(new_answer)
+        user = input(random.choice(responses))
+        user = user.lower()
+        if (user in answers):
+            new_answer = input(
+                "good to know " + (random.choice(responses)) + "\n")
+            answers.append(new_answer)
 
-        if (user in (subjects)):
-            new_subject = input ("oh! how nice! tell me more about it.")
-            subjects.append(new_subject)
+            found_subject = False
+            for sub in subjects:
+                if sub in new_answer:
+                    found_subject = True
 
-        else :
-          input("that's so cool! maybe we can do it together sometime")
-
+            if(found_subject):
+                new_subject = input(
+                    "oh! how nice! tell me more about it.\n")
+                subjects.append(new_subject)
+            else:
+                input("that's so cool! maybe we can do it together sometime? ")
